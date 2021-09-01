dd9aa388f77b5944a059afc5660b208aa403d491
SantaKolosovska <santa.kolosovska@gmail.com>
2021-09-01 21:22:05 +0300

    database added

---
+++ src/Database.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+
+public class Database {
+
+    public static void main(String[] args) {
+
+        String databasePath = "jdbc:sqlite:family_heritage.db";
+
+        try {
+            Connection connection = DriverManager.getConnection(databasePath);
+
+            if (connection != null) {
+                DatabaseMetaData metaData = (DatabaseMetaData) connection.getMetaData();
+                System.out.println("Connected to database");
+
+                Statement statement = connection.createStatement();
+                String sqlStatement1 =
+                        "CREATE TABLE IF NOT EXISTS persons" +
+                                "(person_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "name TEXT NOT NULL," +
+                                "surname TEXT NOT NULL," +
+                                "gender TEXT NOT NULL," +
+                                "birth_date DATE," +
+                                "death_date DATE," +
+                                "place_of_birth TEXT NOT NULL)";
+//                                "description TEXT NOT NULL," +
+//                                "profession_id TEXT NOT NULL," +
+//                                "FOREIGN KEY (profession_ID) REFERENCES professions(id))";
+                statement.execute(sqlStatement1);
+
+                String sqlStatement2 =
+                        "CREATE TABLE IF NOT EXISTS relationships" +
+                                "(relationship_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "person_1 TEXT NOT NULL," +
+                                "person_1_id INTEGER," +
+                                "person_2 TEXT NOT NULL," +
+                                "person_2_id INTEGER," +
+                                "relationship_type TEXT NOT NULL," +
+                                "FOREIGN KEY(person_1_id) REFERENCES persons(person_id)," +
+                                "FOREIGN KEY(person_2_id) REFERENCES persons(person_id))";
+
+                statement.execute(sqlStatement2);
+            }
+
+
+        } catch (SQLException exception) {
+            System.out.println("There was an error: " + exception);
+        }
+
+
+    }
+}
+
+
